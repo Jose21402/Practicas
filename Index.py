742810fbf5b72f6ddf0c2213cbafdcedd004c835
Jose21402 <jm202232@gmail.com>
2020-04-03 14:49:23 -0600

    agrego Ttipos_de_Datos

--- Index.py
+++ Index.py
@@ -1,4 +1,18 @@
 """
+author = José Galván
+Estructuras de Comentarios en Python
+"""
+#Esto un comentario 
+print("Creando comentarios")
 
-
+print("Nadie con COVID-19")#Esto es otro Comentario
+#Esto es solo para decir
+#que este comentario no es
+#multilinea
+print("Todos queremos salir")
+print("Pero no se puede")
+"""
+Esto sí es 
+un comentario
+multilinea
 """
